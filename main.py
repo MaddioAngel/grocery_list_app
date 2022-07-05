19c340b143af19c2e9053f79de8c0582530a8f07
MadisonAngel <maddiotomboy@yahoo.com>
2022-07-04 20:30:33 -0700

    adding json for each meal

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ import random
 days_of_the_week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
 def get_breakfast(num):
-    return num
+    data = ""
+    with open('/Users/madison/Desktop/Code/grocery_list_app/recipes/breakfast/breakfast.json') as file_:
+        data = json.load(file_)[str(num)]
+    return data
 
 def get_lunch(num):
     return num
@@ -26,12 +29,15 @@ if __name__ == "__main__":
     print("and the ammount of money you will need")
     print("generating...")
     print()
-    numbers_4_generation = [ random.sample(range(11), 7) , random.sample(range(11), 7), random.sample(range(11), 7) ]
+    numbers_4_generation = [ random.sample(range(1,11), 7) , random.sample(range(1,11), 7), random.sample(range(1,11), 7) ]
     for i in range(0,7):
         print("{}: ".format(days_of_the_week[i].upper()))
-        print("BREAKFAST: {}".format(get_breakfast(numbers_4_generation[0][i])))
-        print("LUNCH: {}".format(get_lunch(numbers_4_generation[1][i])))
-        print("DINNER: {}".format(get_dinner(numbers_4_generation[2][i])))
+        breakfast = get_breakfast(numbers_4_generation[0][i])
+        lunch = get_lunch(numbers_4_generation[1][i])
+        dinner = get_dinner(numbers_4_generation[2][i])
+        print("BREAKFAST: {}".format(breakfast["name"]))
+        print("LUNCH: {}".format(lunch))
+        print("DINNER: {}".format(dinner))
         print()
 
 
